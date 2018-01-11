829355f1fe2c9de684d37bce2b9a92ab3b2d3a3a
angrha <angrha@gmail.com>
2018-01-11 19:22:16 +0700

    add google function

---
+++ src/components/Play.vue
@@ -0,0 +1,23 @@
+<template>
+  
+</template>
+
+<script>
+const db = this.$firebase
+export default {
+  firebase () {
+    return {
+      anArray: db.ref('rooms')
+    }
+  },
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
