ecf5f82fd6b4a311eae5c143e05111b8ae6f1ad2
azhariemuhammad <azharie_muhammad@live.com>
2018-01-12 16:06:12 +0700

    ready to deploy

--- src/assets/dummy.js
+++ src/assets/dummy.js
@@ -38,8 +38,56 @@ export default {
       image: "https://hello-pet.com/assets/uploads/2016/11/tentara-nasional-indonesia-angkatan-darat.jpg"
     },
     {
-      kata: "tukul",
+      kata: "tukul arwana",
       image: "http://www.lihat.co.id/wp-content/uploads/2016/08/tukul-1.jpg"
+    },
+    {
+      kata: "tupperware",
+      image: "https://rukminim1.flixcart.com/image/1664/1664/lunch-box/z/w/q/tupperware-tp-187-original-imaekghkcuyrkx25.jpeg?q=90"
+    },
+    {
+      kata: "maling jemuran",
+      image: "https://assets-a1.kompasiana.com/items/album/2016/08/30/50maling-jemuran-x-57c59079be22bd663b9af7db.jpg"
+    },
+    {
+      kata: "gorengan",
+      image: "https://giegulali.files.wordpress.com/2016/03/wp-14577134306911.jpeg"
+    },
+    {
+      kata: "poligami",
+      image: "http://cdn2.tstatic.net/kaltim/foto/bank/images/poligami-ilustrasi_20170822_080043.jpg"
+    },
+    {
+      kata: "andhika",
+      image: "http://cdn2.tstatic.net/style/foto/bank/images/andhika-ex-kangen-band_20170327_171902.jpg"
+    },
+    {
+      kata: "kamen rider",
+      image: "https://orig00.deviantart.net/55ab/f/2010/082/a/8/kamen_rider_obama_by_pyrodarkfire.jpg"
+    },
+    {
+      kata: "senyum",
+      image: "https://memegenerator.net/img/images/600x600/12619505/ayo-sini-sama-om.jpg"
+    },
+    {
+      kata: "kak seto",
+      image: "https://scontent-sea1-1.cdninstagram.com/t51.2885-15/e35/25014255_197930684088429_4294066920530903040_n.jpg?ig_cache_key=MTY2MzgzOTc2MjAwNTc4OTMxNQ%3D%3D.2&se=7"
+    },
+    {
+      kata: "dukun",
+      image: "http://4.bp.blogspot.com/-AMhzaSVPGK8/UB60d8hg5RI/AAAAAAAAABk/UmphzD06fm0/s1600/ada_mbah_dukun________by_anggalanell.jpg"
+    },
+    {
+      kata: "nenek lincah",
+      image: "http://www.gregetan.com/wp-content/uploads/2015/09/Kohama-Island.jpg"
+    },
+    {
+      kata: "tukang sayur",
+      image: "https://aripitstop.files.wordpress.com/2014/11/tukang-sayur-r25-1.jpg"
+    },
+    {
+      kata: "bujangan",
+      image: "https://i.ytimg.com/vi/dZ2h9an76HI/hqdefault.jpg"
     }
   ]
 }
